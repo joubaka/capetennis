14833473f68deebc30f294a185a020c6301d1b06
pajoubert <hermanustennisacademy@gmail.com>
2026-05-28 16:33:10 +0200

    up

---
+++ database/migrations/2026_05_28_163222_add_missing_columns_to_registration_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('registration_orders', function (Blueprint $table) {
+            if (!Schema::hasColumn('registration_orders', 'payment_method')) {
+                $table->string('payment_method')->nullable()->after('pay_status');
+            }
+            if (!Schema::hasColumn('registration_orders', 'wallet_transaction_id')) {
+                $table->unsignedBigInteger('wallet_transaction_id')->nullable()->after('payment_method');
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('registration_orders', function (Blueprint $table) {
+            $table->dropColumn(['payment_method', 'wallet_transaction_id']);
+        });
+    }
+};
